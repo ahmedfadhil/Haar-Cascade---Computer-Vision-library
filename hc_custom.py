488d92c677ecc26b9bc20f0a78d82f289af49c7b
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-08 17:03:43 +0100

    filtering the neg, pos and uglies

--- hc_custom.py
+++ hc_custom.py
@@ -24,4 +24,39 @@ def store_raw_images():
 
             except Exception as e:
                 print(str(e))
+
+
+def find_uglies():
+    for file_type in ['neg']:
+        for img in os.listdir((file_type)):
+            for uglies in os.listdir('uglies'):
+                try:
+                    current_image_path = str(file_type) + '/' + str(img)
+                    ugly = cv2.imread('uglies/' + str(ugly))
+                    question = cv2.imread(current_image_path)
+
+                    if ugly.shape == question.shape and not (np.bitwise_xor(ugly, question).any()):
+                        print('dam girl you ugly')
+                        print(current_image_path)
+                        os.remove(current_image_path)
+                except Exception as e:
+
+                    print(str(e))
+
+
+def create_post_n_neg():
+    for file_type in ['neg']:
+        for img in os.listdir(file_type):
+            if file_type == 'neg':
+                line = file_type + '/' + img + '\n'
+                with open('bg.txt', 'a') as f:
+                    f.write(line)
+            elif file_type == 'pos':
+                line = file_type + '/' + img + '1 0 0 50 50 \n'
+                with open('infor.dat', 'a') as f:
+                    f.write(line)
+
+
+create_post_n_neg()
+find_uglies()
 store_raw_images()

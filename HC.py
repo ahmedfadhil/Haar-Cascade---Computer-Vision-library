9fbfca8cbc40d980260aba063f73aec136fb18bc
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-08 12:03:48 +0100

    final commit

--- HC.py
+++ HC.py
@@ -1,11 +1,7 @@
-import numpy as np
 import cv2
+import numpy as np
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
@@ -19,7 +15,6 @@ while 1:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
-
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
@@ -28,6 +23,5 @@ while 1:
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
-
 cap.release()
 cv2.destroyAllWindows()
